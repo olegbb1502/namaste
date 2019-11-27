1e7993b28ce31f3b05fb1e62da37af2b13e3a9ff
olegbb1502 <bolobanoleg@gmail.com>
2019-11-28 00:44:47 +0200

    new pages

--- build/js/app.js
+++ build/js/app.js
@@ -8,6 +8,7 @@
   document.addEventListener("DOMContentLoaded", function () {
     menuHandler();
     $('.carousel').slick();
+    submittedForm();
   }); // menu handler
 
   var menuHandler = function menuHandler() {
@@ -16,6 +17,35 @@
     menuButton.addEventListener('click', function () {
       menuButton.classList.toggle('open');
       menu.classList.toggle('open');
+    });
+  };
+
+  var submittedForm = function submittedForm() {
+    var data = {};
+    var formAlert = document.querySelector('.form-alert');
+    var formInputs = document.querySelectorAll('.row input');
+    var submitted = document.querySelector('.submit');
+    submitted.addEventListener('click', function () {
+      formInputs.forEach(function (input) {
+        var name = input.name;
+        var value = input.value;
+
+        if (value !== '') {
+          data[name] = value;
+          input.classList.remove('invalid');
+          formAlert.style = "background: #2ecc71";
+          formAlert.innerHTML = "<span>Successful send!</span>";
+        } else {
+          input.classList.add('invalid');
+          formAlert.style = "background: #e74c3c";
+          formAlert.innerHTML = "<span>Invalid values!</span>";
+        }
+      });
+      console.log(data);
+      formAlert.classList.add('splash');
+      setTimeout(function () {
+        return formAlert.classList.remove('splash');
+      }, 3000);
     });
   };
 });
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 	menuHandler();
 	$('.carousel').slick();
+    submittedForm();
 });
 
 // menu handler
@@ -11,5 +12,34 @@ const menuHandler = () => {
 	menuButton.addEventListener('click', ()=> {
 		menuButton.classList.toggle('open');
 		menu.classList.toggle('open');
+	})
+}
+
+const submittedForm = () => {
+	const data = {};
+	const formAlert = document.querySelector('.form-alert');
+	const formInputs = document.querySelectorAll('.row input');
+	const submitted = document.querySelector('.submit');
+
+	submitted.addEventListener('click', ()=>{
+		formInputs.forEach(input=>{
+			let name = input.name;
+			let value = input.value;
+			if(value !== ''){
+                data[name] = value;
+                input.classList.remove('invalid');
+                formAlert.style = "background: #2ecc71"
+                formAlert.innerHTML = "<span>Successful send!</span>";
+			} else {
+				input.classList.add('invalid');
+				formAlert.style = "background: #e74c3c"
+                formAlert.innerHTML = "<span>Invalid values!</span>";
+			}
+		})
+
+		console.log(data);
+
+		formAlert.classList.add('splash');
+		setTimeout(()=>formAlert.classList.remove('splash'), 3000);
 	})
 }
