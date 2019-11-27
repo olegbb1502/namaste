6001a0a3a766458a20c02fb81b15c11cd929531a
olegbb1502 <bolobanoleg@gmail.com>
2019-11-28 00:06:19 +0200

    new pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-import $ from "jquery";
+document.addEventListener("DOMContentLoaded", () => {
+	menuHandler();
+	$('.carousel').slick();
+});
 
-$(document).ready(function(){
-	$('body').css('color', 'red');
-})
+// menu handler
+const menuHandler = () => {
+	const menuButton = document.querySelector('.mob-button');
+	const menu = document.querySelector('nav ul');
+
+	menuButton.addEventListener('click', ()=> {
+		menuButton.classList.toggle('open');
+		menu.classList.toggle('open');
+	})
+}
